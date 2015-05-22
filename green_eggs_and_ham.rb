5c097ebb1eb15225f6c07d55f7c1205a2a887683
Aaron Harvey <harvnlenny@gmail.com>
2015-05-22 11:51:06 -0400

    passed 2

--- green_eggs_and_ham.rb
+++ green_eggs_and_ham.rb
@@ -3,4 +3,20 @@ class GreenEggsAndHam
   def initialize
     @text = File.read('green_eggs_and_ham.txt')
   end
+  
+  def words
+    @text.split.map do |word|
+      word.downcase.gsub(/[^a-z-]/, '')
+    end
+  end
+
+  def word_count
+    words.count
+  end
+
+  def sorted_unique_words
+    words.uniq.sort
+  end
 end
+
+
